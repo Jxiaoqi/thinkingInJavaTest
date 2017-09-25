d596c3e1f2ef0c4f089c1f7d07cea55cdaf32c08
Jxiaoqi <jiayuhao1993@yeah.net>
2017-09-25 16:57:02 +0800

    加入maven支持

--- src/main/java/access/Cake.java
+++ src/main/java/access/Cake.java
@@ -1,4 +1,5 @@
 package access;
+
 //: access/Cake.java
 // Accesses a class in a separate compilation unit.
 
@@ -7,6 +8,12 @@ class Cake {
     Pie x = new Pie();
     x.f();
   }
+
+
+  public void test() {
+    System.out.println("aaaa");
+  }
+
 } /* Output:
 Pie.f()
 *///:~
---
+++ src/main/java/java8feature/ConsumerTest.java
@@ -0,0 +1,24 @@
+package java8feature;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * 演示函数式接口Consumer
+ */
+public class ConsumerTest {
+
+    public static <T> void forEach (List<T> list, Consumer<T> consumer) {
+        for (T t : list) {
+            consumer.accept(t);
+        }
+    }
+
+    public static void main (String[] args) {
+        List<String> list = Arrays.asList("a","v","b","e");
+        forEach(list, (String s) -> System.out.println(s));
+
+    }
+
+}
